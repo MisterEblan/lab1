b629b086a531b8983383ff4de94972bb775877fc
MisterEblan <boy4rov.da@gmail.com>
2025-11-08 21:33:48 +0300

    feat: function decomposed

--- main.py
+++ main.py
@@ -2,8 +2,25 @@ from math import sin, cos
 
 eps: float = 1e-2
 
-def main():
-    x = float(input("Ввод >> "))
+def f(x: float) -> float:
+    """
+    Вычисляет значение выражения
+    $$ \\frac{(\\sin(x) + \\cos (2x) )^2}{\\sin(2x)} $$
+
+    Область определения:
+        Вся вещественная прямая, кроме точек
+        $\\frac{\\pi n}{2}, n \\in \\mathbb{N}$.
+
+    Args:
+        x: значение аргумента.
+
+    Returns:
+        Вычисленное значение.
+
+    Raises:
+        ZeroDivisionError: если подать значение не из области определения.
+    """
+
     if abs(denominator := sin(2 * x)) < eps:
         raise ZeroDivisionError("Знаменатель близок к нулю!")
 
@@ -11,8 +28,15 @@ def main():
 
     f_x = enumerator / denominator
 
+    return f_x
+
+def main():
+    x = float(input("Ввод >> "))
+
+    result = f(x)
+
     print(f"x={x}")
-    print(f"f={f_x:.6}")
+    print(f"f={result:.6}")
 
 if __name__ == "__main__":
     main()
